9a266b2a4cddba54515fa520afb149290ab74270
Alesandr Sokirka <73969654+alesandrsokirka@users.noreply.github.com>
2024-06-13 15:12:56 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as pltt
 import os
 import re
 import streamlit as st
@@ -7,7 +6,6 @@ import pandas as pd
 from transformers import pipeline
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pdd
 
 st.title('Анализатор комментариев :red[YouTube] :sunglasses:')
 
